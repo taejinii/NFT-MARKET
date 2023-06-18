3a34b5dcd8f92c692fafde07b36c8205c30d8209
taejinii <xowls177@gmail.com>
2023-06-18 22:06:40 +0900

    design: NFT 카드 디자인 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import Card from "@/components/Card";
 import FirstSection from "@/components/Landing/FirstSection";
 import SecontSection from "@/components/Landing/SecontSection";
+import ThirdSection from "@/components/Landing/ThirdSection";
 export default function Home() {
   return (
     <main>
       <FirstSection />
       <SecontSection />
+      <ThirdSection />
+      <Card />
     </main>
   );
 }

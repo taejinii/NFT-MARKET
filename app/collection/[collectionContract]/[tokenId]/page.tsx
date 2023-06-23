44889f3df4dab2e33d1a492bb49b3154aff58e7c
taejinii <xowls177@gmail.com>
2023-06-23 22:59:36 +0900

    Feat: Accordion 기능 구현

--- app/collection/[collectionContract]/[tokenId]/page.tsx
+++ app/collection/[collectionContract]/[tokenId]/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { getNFTInfoDetail } from "@/api/CollectionAPI";
 import NftInfo from "@/components/page/Collection/NftDetail/NftInfo";
 import TraitsList from "@/components/page/Collection/NftDetail/TraitsList";
-
+import Accordion from "@/components/ui/Accordion/index";
 interface ParamsType {
   collectionContract: string;
   tokenId: string;
@@ -13,13 +13,13 @@ export default async function NFTDetail({ params }: { params: ParamsType }) {
   const info = await getNFTInfoDetail(collectionContract, tokenId);
   const { image, traits } = info;
   return (
-    <div className="flex flex-col items-center justify-center w-full py-10 m-auto max-w-7xl">
-      <section className="flex w-full">
-        <div className="pr-4 border-r-2 border-black ">
+    <div className="flex flex-col items-center justify-center w-full gap-5 py-10 m-auto max-w-7xl">
+      <section className="flex flex-col w-full gap-5 md:flex-row">
+        <div className="pr-4 border-r-2 border-black max-md:order-2">
           <NftInfo info={info} />
           <TraitsList traits={traits} />
         </div>
-        <div className="flex items-center justify-center w-full ">
+        <div className="flex items-center justify-center w-full max-md:order-1">
           <Image
             src={image}
             alt="NFT Image"
@@ -29,8 +29,9 @@ export default async function NFTDetail({ params }: { params: ParamsType }) {
           />
         </div>
       </section>
-
-      <div className="flex items-start justify-start w-full">1</div>
+      <Accordion title="Item Activity">
+        <div className="w-full border-b-[1px] p-4 border-black">1</div>
+      </Accordion>
     </div>
   );
 }
---
+++ components/ui/Accordion/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useState, ReactNode } from "react";
+interface AccordionType {
+  title: string;
+  children: ReactNode;
+}
+export default function Accordion({ title, children }: AccordionType) {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleAccordion = () => [setIsOpen(!isOpen)];
+  return (
+    <div className="flex flex-col items-start justify-start w-full border-2 border-black rounded-md">
+      <div className="w-full text-2xl font-bold" onClick={handleAccordion}>
+        <div className="p-4 border-b-2 border-black">{title}</div>
+      </div>
+      {isOpen && (
+        <div className="w-full overflow-auto max-h-52">{children}</div>
+      )}
+    </div>
+  );
+}

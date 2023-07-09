6c86eff3a994f08e46157d42452c0f925e5982be
taejinii <xowls177@gmail.com>
2023-07-10 01:08:48 +0900

    Update: 지갑관련 코드 및 의존성 제거

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,19 +1,9 @@
 "use client";
-import "@rainbow-me/rainbowkit/styles.css";
-
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { WagmiConfig, createConfig, configureChains } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, zora } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
-import { useState, PropsWithChildren, useEffect } from "react";
+import { useState, PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { store } from "@/store";
 export default function Providers({ children }: PropsWithChildren) {
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
   const [queryClient] = useState(
     () =>
       new QueryClient({
@@ -25,33 +15,9 @@ export default function Providers({ children }: PropsWithChildren) {
       })
   );
 
-  const { chains, publicClient, webSocketPublicClient } = configureChains(
-    [mainnet, polygon, optimism, arbitrum, zora],
-    [publicProvider()]
-  );
-  const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string;
-  const { connectors } = getDefaultWallets({
-    appName: "QWERO",
-    projectId,
-    chains,
-  });
-
-  const config = createConfig({
-    autoConnect: true,
-    publicClient,
-    connectors,
-    webSocketPublicClient,
-  });
-
   return (
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <WagmiConfig config={config}>
-          <RainbowKitProvider chains={chains}>
-            {mounted && children}
-          </RainbowKitProvider>
-        </WagmiConfig>
-      </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </Provider>
   );
 }
